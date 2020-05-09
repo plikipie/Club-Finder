a67819ecbfeda50f59946692e8154a12395d60d0
plikipe <plikipie@gmail.com>
2020-05-09 18:25:51 +0700

    first commit

---
+++ src/script/data/data-source.js
@@ -0,0 +1,65 @@
+import clubs from "./clubs.js";
+// KODE AWAL
+// function DataSource(onSuccess, onFailed) {
+//   this.onSuccess = onSuccess;
+//   this.onFailed = onFailed;
+// }
+
+// DataSource.prototype.searchClub = function(keyword) {
+//     const filteredClubs = clubs.filter((club) =>
+//         club.name.toUpperCase().includes(keyword.toUpperCase())
+//     );
+
+//     if (filteredClubs.length) {
+//         this.onSuccess(filteredClubs);
+//     } else {
+//         this.onFailed(`${keyword} is not found`);
+//     }
+// };
+
+class DataSource {
+    constructor(onSuccess, onFailed) {
+        this.onSuccess = onSuccess;
+        this.onFailed = onFailed;
+    }
+
+    searchClub(keyword) {
+        const filteredClubs = clubs.filter((club) =>
+            club.name.toUpperCase().includes(keyword.toUpperCase())
+        );
+
+        if (filteredClubs.length) {
+            this.onSuccess(filteredClubs);
+        } else {
+            this.onFailed(`${keyword} is not found`);
+        }
+    }
+}
+export default DataSource;
+
+// TODO : AKAN LEBIH SIMPLE MENGGUNAKAN PROMISE
+// class DataSource {
+//     searchClub(keyword) {
+//         const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+
+//         if (filteredClubs.length) {
+//             this.onSuccess(filteredClubs);
+//         } else {
+//             this.onFailed(`${keyword} is not found"`);
+//         }
+//     }
+// }
+
+// TODO : MENGGUNAKAN PROMISE STATIC WAJIB MENGUBAH DI MAIN.JS
+// class DataSource {
+//     static searchClub(keyword) {
+//         return new Promise((resolve, reject) => {
+//             const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+//             if (filteredClubs.length) {
+//                 resolve(filteredClubs);
+//             } else {
+//                 reject(`${keyword} is not found"`);
+//             }
+//         });
+//     }
+// }
---
+++ src/script/view/main.js
@@ -0,0 +1,56 @@
+import DataSource from "../data/data-source";
+
+const main = () => {
+  const searchElement = document.querySelector("#searchElement");
+  const buttonSearchElement = document.querySelector("#searchButtonElement");
+  const clubListElement = document.querySelector("#clubList");
+
+  // KODE AWAL
+  const onButtonSearchClicked = () => {
+    const dataSource = new DataSource(renderResult, fallbackResult);
+    dataSource.searchClub(searchElement.value);
+  };
+  // TODO : KODE DIBAWAH INI APABILA MEMAKAI PROMISE STATIC
+  //   const onButtonSearchClicked = () => {
+  //     DataSource.searchClub(searchElement.value)
+  //         .then(renderResult)
+  //         .catch(fallbackResult)
+  // };
+
+  //TODO: BISA JUGA MENGGUNAKAN ASYNC / AWAIT (PROMISE STATIC)
+  //   const onButtonSearchClicked = async () => {
+  //     try{
+  //         const result = await DataSource.searchClub(searchElement.value);
+  //         renderResult(result);
+  //     } catch (message) {
+  //         fallbackResult(message)
+  //     }
+  //  };
+
+  const renderResult = (results) => {
+    clubListElement.innerHTML = "";
+    results.forEach((club) => {
+      const { name, fanArt, description } = club;
+
+      const clubElement = document.createElement("div");
+      clubElement.setAttribute("class", "club");
+
+      clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+        <div class="club-info">
+            <h2>${name}</h2> 
+            <p>${description}</p> 
+        </div>`;
+
+      clubListElement.appendChild(clubElement); 
+    });
+  };
+
+  const fallbackResult = (message) => {
+    clubListElement.innerHTML = "";
+    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+  };
+
+  buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+};
+
+export default main;
