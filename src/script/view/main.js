19d450ebc5cf080161cc3aafb8777873bc06dd56
plikipe <plikipie@gmail.com>
2020-05-20 14:28:08 +0700

    Mengubah search-bar menjadi web-component(Belom work)

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,56 +1,59 @@
+import "../component/search-bar.js";
 import DataSource from "../data/data-source.js";
 
 const main = () => {
-  const searchElement = document.querySelector("#searchElement");
-  const buttonSearchElement = document.querySelector("#searchButtonElement");
-  const clubListElement = document.querySelector("#clubList");
+    const searchElement = document.querySelector("search-bar");
+    const clubListElement = document.querySelector("#clubList");
 
-  // KODE AWAL
-  const onButtonSearchClicked = () => {
-    const dataSource = new DataSource(renderResult, fallbackResult);
-    dataSource.searchClub(searchElement.value);
-  };
-  // TODO : KODE DIBAWAH INI APABILA MEMAKAI PROMISE STATIC
-  //   const onButtonSearchClicked = () => {
-  //     DataSource.searchClub(searchElement.value)
-  //         .then(renderResult)
-  //         .catch(fallbackResult)
-  // };
+    // KODE AWAL
+    const onButtonSearchClicked = async() => {
+        try {
+            const result = await DataSource.searchClub(searchElement.value);
+            renderResult(result);
+        } catch (message) {
+            fallbackResult(message);
+        }
+    };
+    // TODO : KODE DIBAWAH INI APABILA MEMAKAI PROMISE STATIC
+    //   const onButtonSearchClicked = () => {
+    //     DataSource.searchClub(searchElement.value)
+    //         .then(renderResult)
+    //         .catch(fallbackResult)
+    // };
 
-  //TODO: BISA JUGA MENGGUNAKAN ASYNC / AWAIT (PROMISE STATIC)
-  //   const onButtonSearchClicked = async () => {
-  //     try{
-  //         const result = await DataSource.searchClub(searchElement.value);
-  //         renderResult(result);
-  //     } catch (message) {
-  //         fallbackResult(message)
-  //     }
-  //  };
+    //TODO: BISA JUGA MENGGUNAK0AN ASYNC / AWAIT (PROMISE STATIC)
+    //   const onButtonSearchClicked = async () => {
+    //     try{
+    //         const result = await DataSource.searchClub(searchElement.value);
+    //         renderResult(result);
+    //     } catch (message) {
+    //         fallbackResult(message)
+    //     }
+    //  };
+    const renderResult = (results) => {
+        clubListElement.innerHTML = "";
+        results.forEach((club) => {
+            const { name, fanArt, description } = club;
 
-  const renderResult = (results) => {
-    clubListElement.innerHTML = "";
-    results.forEach((club) => {
-      const { name, fanArt, description } = club;
+            const clubElement = document.createElement("div");
+            clubElement.setAttribute("class", "club");
 
-      const clubElement = document.createElement("div");
-      clubElement.setAttribute("class", "club");
-
-      clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+            clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
         <div class="club-info">
             <h2>${name}</h2> 
             <p>${description}</p> 
         </div>`;
 
-      clubListElement.appendChild(clubElement);
-    });
-  };
+            clubListElement.appendChild(clubElement);
+        });
+    };
 
-  const fallbackResult = (message) => {
-    clubListElement.innerHTML = "";
-    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
-  };
+    const fallbackResult = (message) => {
+        clubListElement.innerHTML = "";
+        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    };
 
-  buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+    searchElement.clickEvent = onButtonSearchClicked;
 };
 
 export default main;
